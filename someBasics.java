776eb4dcf11717ee8d9f8a1a7d69dcef947b0407
mukhtar4532 <mukhtarhussain4532@gmail.com>
2023-12-26 12:29:09 +0530

    first commit

---
+++ someBasics.java
@@ -0,0 +1,93 @@
+import java.util.*;
+
+public class someBasics {
+   public static void main(String args[]) {
+      Scanner sc = new Scanner(System.in);
+      // int a = sc.nextInt();
+      // int b = sc.nextInt();
+      // int c = sc.nextInt();
+      // System.out.println("The average of your no. is "+((a+b+c)/3));
+
+      // System.out.println("Enter the side of the square ");
+      // int side = sc.nextInt();
+      // System.out.println("The of the squar is "+ (side*side));
+
+      // System.out.println("Enter the cost of the pencil, pen and eraser ");
+      // float pencilCost = sc.nextFloat();
+      // float penCost = sc.nextFloat();
+      // float eraserCost = sc.nextFloat();
+      // float totalCost = pencilCost + penCost + eraserCost ;
+      // System.out.println("The total cost of these things is " + totalCost);
+      // System.out.println("Total cost with GST " + ((totalCost +
+      // (0.18f)*totalCost)));
+
+      // System.out.println("Enter no. to check a no. is positive or negative ");
+      // int num = sc. nextInt();
+      // if (num > 0 ) {
+      // System.out.println(num + " is positive");
+      // }else if (num < 0) {
+      // System.out.println(num + " is negative");
+      // }else{
+      // System.out.println(num + " is neither postive nor negative because it is
+      // zero.");
+      // }
+
+      // System.out.println("Enter a no. from 1 to 7 to print a days according to no.
+      // ");
+      // int day = sc. nextInt();
+      // switch (day) {
+      // case 1: System.out.println("Monday");
+      // break;
+      // case 2: System.out.println("Tuesday");
+      // break;
+      // case 3: System.out.println("Wednesday");
+      // break;
+      // case 4: System.out.println("Thursday");
+      // break;
+      // case 5: System.out.println("Friday");
+      // break;
+      // case 6: System.out.println("Saturday");
+      // break;
+      // case 7: System.out.println("Sunday");
+      // break;
+
+      // default:
+      // break;
+      // }
+
+      // int choice;
+      // int num ;
+      // do {
+      //    System.out.println("Enter a no. ");
+      //    num = sc.nextInt();
+      //    if (num % 2 == 0 ) {
+      //       System.out.println(num + " no. is even");
+      //    }else{
+      //       System.out.println(num + " no. is odd ");
+      //    }
+      //    System.out.println("Do you want continue enter 1 otherwise 0 ");
+      //    choice = sc.nextInt();
+      //    // if (choice == 0) {
+      //    //   break; 
+      //    // }
+      // } while (choice==1);
+
+      // int num, fact = 1 ;
+      // System.out.println("Enter no. for the factorial ");
+      // num = sc. nextInt();
+      // for (int i = 1; i <= num; i++) {
+      //    fact *= i;
+      // }
+      // System.out.println("The of the no. is "+fact);
+
+      // System.out.println("Enter no. of the table ");
+      // int num = sc.nextInt();
+      // System.out.println("Table is ");
+      // for (int i = 1; i <= 10; i++) {
+      //    System.out.println(num + " X " + i + " = " + (i*num));
+      // }
+
+      
+      sc.close();
+   }
+}
